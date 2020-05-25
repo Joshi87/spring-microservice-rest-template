0c61a706470d27d3301b96fa569eebae80c315b0
Joshi <shrinivas87.joshi@gmail.com>
2020-05-26 00:29:40 +0530

    Initial Commit

---
+++ src/main/java/org/joshi/springmicroserviceresttemplate/SpringMicroserviceRestTemplateApplication.java
@@ -0,0 +1,13 @@
+package org.joshi.springmicroserviceresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMicroserviceRestTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringMicroserviceRestTemplateApplication.class, args);
+    }
+
+}
